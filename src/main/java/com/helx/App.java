fb1a3c784acb55f18719e8af02fe59aa6339e890
helixin125 <992489930@qq.com>
2019-10-02 14:29:27 +0800

    AdminUi监控Actuator

---
+++ src/main/java/com/helx/App.java
@@ -0,0 +1,17 @@
+package com.helx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
